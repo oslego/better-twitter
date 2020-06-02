5f7fafd2ecad8aaf64280b11802ae15ebf119df0
Razvan Caliman <razvan.caliman+dev@gmail.com>
2020-06-02 15:18:19 +0200

    Hide Who To Follow recommendations when they appear in the main timeline. Fix #4

--- content.js
+++ content.js
@@ -29,14 +29,14 @@ chrome.storage.onChanged.addListener((changes, area) => {
 The new Twitter interface (circa 2019) uses auto-generated markup
 with obfuscated class names which are not guaranteed to be stable over time.
 
-In order to remove unwanted parts of the UI when and if they appear,
+In order to remove unwanted parts of the UI when they appear,
 we use CSS selectors in the stylesheet (content.css) to identify
-areas of interest (mostly by `aria-label` attribute contents which are
-more likely to be stable over time), then apply a short fake animation to them.
+areas of interest, then apply a short fake animation to them.
 
-Here in JavaScript we listen for all `animationstart` events, check if they match
+Here in JavaScript, we listen for all `animationstart` events, check if they match
 one of the areas of interest (`AnimationEvent.target` points to the element matched
-by the CSS seletor), then take further action against its ancestor node.
+by the CSS seletor), then mark the target element or one of its ancestors. This marker
+then causes a CSS selector from content.css to match and hide the intended element.
 
 This is a convoluted but viable workaround for missing ancestor selectors in CSS.
 */
@@ -49,6 +49,10 @@ document.addEventListener('animationstart', (e) => {
     case "bt-marker-nopromoted":
       // Mark tweet container for the "promoted" SVG icon.
       e.target.closest('[data-testid="tweet"]').classList.add(e.animationName)
+      break;
+    case "bt-marker-timeline":
+      // Mark the main timeline container
+      e.target.classList.add(e.animationName)
       break;
   }
 });
